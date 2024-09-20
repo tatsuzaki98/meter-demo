7c3c16753537ef25d55eb28605fdce3a17e02146
tatsuzaki98 <tatsuzaki98@gmail.com>
2024-09-20 20:58:43 +0900

    baseを変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// baseを'/meter-demo/'に指定する
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/meter-demo/',
 })
